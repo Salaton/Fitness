c2e449fb086a1687d9e4116b5ca602cb7a727785
Sala <elvisdenis056@gmail.com>
2019-03-24 13:36:36 +0300

    Added reCAPTCHA V2

--- app.js
+++ app.js
@@ -72,6 +72,34 @@ app.get("/", function(req, res) {
 	});
 });
 
+app.post("/", function(req, res) {
+	if (req.body.captcha === undefined || req.body.captcha === "" || req.body.captcha === null) {
+		return res.json({ success: false, msg: "Please select captcha" });
+	}
+
+	//secret key
+	const secretKey = "6LdvnJkUAAAAABdkQIzWCnGmB8TxHUj6g8FJxAbp";
+
+	// Verify URL
+	const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${
+		req.body.captcha
+	}&remoteip=${req.connection.remoteAddress}`;
+
+	// Make Request To VerifyURL
+	request(verifyUrl, (err, response, body) => {
+		body = JSON.parse(body);
+		console.log(body);
+
+		// If Not Successful
+		if (body.success !== undefined && !body.success) {
+			return res.json({ success: false, msg: "Failed captcha verification" });
+		}
+
+		//If Successful
+		return res.json({ success: true, msg: "Captcha passed" });
+	});
+});
+
 app.get("/classes", function(req, res) {
 	Class.find({}, function(err, allClass) {
 		if (err) {
